448fd5f7d0599e13fed18ca1363ab75534dd5493
campisano <riccardo.campisano@gmail.com>
2020-08-22 08:57:49 -0300

    feature:    transaction have a value, it cannot be null and only payment transactions can have positive values

--- src/main/java/org/example/java_sample_web_payment_app/domain/Transaction.java
+++ src/main/java/org/example/java_sample_web_payment_app/domain/Transaction.java
@@ -10,11 +10,13 @@ public class Transaction {
 
     private String number;
     private Type type;
+    private Money value;
 
-    public Transaction(String number, Type type) {
-        ensureCreable(number, type);
+    public Transaction(String number, Type type, Money value) {
+        ensureCreable(number, type, value);
         this.number = number;
         this.type = type;
+        this.value = value;
     }
 
     public String getNumber() {
@@ -25,12 +27,29 @@ public class Transaction {
         return type;
     }
 
-    private static void ensureCreable(String number, Type type) {
+    public Money getValue() {
+        return value;
+    }
+
+    private static void ensureCreable(String number, Type type, Money value) {
         if (number == null || number.length() == 0) {
-            throw new IllegalArgumentException(MessageFormat.format("Number {0} is invalid", number));
+            throw new IllegalArgumentException(MessageFormat.format("Number '{0}' is invalid", number));
         }
         if (type == null) {
-            throw new IllegalArgumentException("Type is null");
+            throw new IllegalArgumentException("Type 'null' is invalid");
+        }
+        if (value == null) {
+            throw new IllegalArgumentException("Value 'null' is invalid");
+        }
+
+        if (type.equals(Type.PAYMENT)) {
+            if (value.isNegative()) {
+                throw new IllegalArgumentException("Payment transaction must have a positive value");
+            }
+        } else {
+            if (value.isPositive()) {
+                throw new IllegalArgumentException("Only Payment transaction can have a positive value");
+            }
         }
     }
 }
--- src/test/java/org/example/java_sample_web_payment_app/domain/TransactionTest.java
+++ src/test/java/org/example/java_sample_web_payment_app/domain/TransactionTest.java
@@ -6,31 +6,91 @@ import org.junit.jupiter.api.Test;
 public class TransactionTest {
 
     @Test
-    public void test_creation() {
-        Transaction transaction = new Transaction("number", Transaction.Type.CASH);
+    public void test_creation_cash() {
+        Transaction transaction = new Transaction("number", Transaction.Type.CASH, new Money(-100));
 
         Assertions.assertEquals("number", transaction.getNumber());
         Assertions.assertEquals(Transaction.Type.CASH, transaction.getType());
     }
 
     @Test
+    public void test_creation_installment() {
+        Transaction transaction = new Transaction("number", Transaction.Type.INSTALLMENT, new Money(-10));
+
+        Assertions.assertEquals("number", transaction.getNumber());
+        Assertions.assertEquals(Transaction.Type.INSTALLMENT, transaction.getType());
+        Assertions.assertEquals("number", transaction.getNumber());
+    }
+
+    @Test
+    public void test_creation_installment_withdrawal() {
+        Transaction transaction = new Transaction("number", Transaction.Type.WITHDRAWAL, new Money(-100));
+
+        Assertions.assertEquals("number", transaction.getNumber());
+        Assertions.assertEquals(Transaction.Type.WITHDRAWAL, transaction.getType());
+    }
+
+    @Test
+    public void test_creation_installment_payment() {
+        Transaction transaction = new Transaction("number", Transaction.Type.PAYMENT, new Money(100));
+
+        Assertions.assertEquals("number", transaction.getNumber());
+        Assertions.assertEquals(Transaction.Type.PAYMENT, transaction.getType());
+    }
+
+    @Test
     public void test_creation_null_number() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Transaction(null, Transaction.Type.CASH);
+            new Transaction(null, Transaction.Type.CASH, new Money(-1));
         });
     }
 
     @Test
     public void test_creation_empty_number() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Transaction("", Transaction.Type.CASH);
+            new Transaction("", Transaction.Type.CASH, new Money(-1));
         });
     }
 
     @Test
     public void test_creation_null_type() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Transaction("number", null);
+            new Transaction("number", null, new Money(-1));
+        });
+    }
+
+    @Test
+    public void test_creation_null_value() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Transaction("number", Transaction.Type.CASH, null);
+        });
+    }
+
+    @Test
+    public void test_creation_cash_positive() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Transaction("number", Transaction.Type.CASH, new Money(1));
+        });
+    }
+
+    @Test
+    public void test_creation_installment_positive() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Transaction("number", Transaction.Type.INSTALLMENT, new Money(1));
+        });
+    }
+
+    @Test
+    public void test_creation_withdrawal_positive() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Transaction("number", Transaction.Type.WITHDRAWAL, new Money(1));
+        });
+    }
+
+    @Test
+    public void test_creation_payment_negative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Transaction("number", Transaction.Type.PAYMENT, new Money(-1));
         });
     }
 }
