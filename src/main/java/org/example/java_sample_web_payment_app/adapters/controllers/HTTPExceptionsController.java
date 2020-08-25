410797a8470cbfabaa51db0f2c091fab48b80a21
campisano <riccardo.campisano@gmail.com>
2020-08-25 02:21:57 -0300

    feature:    improve HTTPExceptionsController, add integration tests

--- src/main/java/org/example/java_sample_web_payment_app/adapters/controllers/HTTPExceptionsController.java
+++ src/main/java/org/example/java_sample_web_payment_app/adapters/controllers/HTTPExceptionsController.java
@@ -2,13 +2,17 @@ package org.example.java_sample_web_payment_app.adapters.controllers;
 
 import java.util.Objects;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
@@ -16,17 +20,28 @@ public class HTTPExceptionsController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HTTPExceptionsController.class);
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<CustomErrorResponse> handleNotFound(Exception exception, WebRequest request) {
-        LOGGER.error("Request: {}, exception: {}", Objects.toString(request), exception.getMessage());
+    public ResponseEntity<CustomErrorResponse> handleNotFound(Exception exception, HttpServletRequest request) {
+        LOGGER.error("Request: {}, exception: {}", Objects.toString(request), exception.toString());
 
         CustomErrorResponse error = new CustomErrorResponse();
-        error.message = "Not found: " + request.getContextPath();
+        error.message = "Not found: " + request.getRequestURI();
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler({ HttpMessageNotReadableException.class, HttpMediaTypeNotSupportedException.class,
+            HttpRequestMethodNotSupportedException.class })
+    public ResponseEntity<CustomErrorResponse> handleJsonParserException(Exception exception,
+            HttpServletRequest request) {
+        LOGGER.error("Request: {}, exception: {}", Objects.toString(request), exception.toString());
+
+        CustomErrorResponse error = new CustomErrorResponse();
+        error.message = "Bad request";
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<CustomErrorResponse> handleOthers(Exception exception, WebRequest request) {
-        LOGGER.error("Request: {}, exception: {}", Objects.toString(request), exception.getMessage());
+    public ResponseEntity<CustomErrorResponse> handleOthers(Exception exception, HttpServletRequest request) {
+        LOGGER.error("Request: {}, exception: {}", Objects.toString(request), exception.toString());
 
         CustomErrorResponse error = new CustomErrorResponse();
         error.message = "Internal server error";
---
+++ src/test/java/org/example/java_sample_web_payment_app/IT/HTTPExceptionsControllerIntegationTest.java
@@ -0,0 +1,60 @@
+package org.example.java_sample_web_payment_app.IT;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@Testcontainers
+public class HTTPExceptionsControllerIntegationTest {
+
+    @Container
+    public static final PostgreSQLContainer<?> postgresContainer = CustomPostgresqlContainer.getInstance();
+
+    @Autowired
+    private TestRestTemplate rest;
+
+    @Test
+    public void test_not_found() {
+        ResponseEntity<?> response = rest.postForEntity("/not_existent", null, null);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void test_bad_request() {
+        ResponseEntity<?> response = rest.exchange("/accounts", HttpMethod.POST, HttpEntity.EMPTY, Object.class);
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void test_internal_server_error() {
+        ResponseEntity<?> response = rest.exchange("/accounts", HttpMethod.POST, new HttpEntity<>(new BadRequestDTO()),
+                Object.class);
+
+        Assertions.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+}
+
+class BadRequestDTO {
+    public long id;
+}
