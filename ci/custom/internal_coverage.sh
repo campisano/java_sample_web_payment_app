ef00b032a3fe0d1cdd5a5ead151334eb1d2bb752
campisano <riccardo.campisano@gmail.com>
2020-08-26 09:01:17 -0300

    fix:        add curl dependency in CI script, it is used by codecov shell script

--- ci/custom/internal_coverage.sh
+++ ci/custom/internal_coverage.sh
@@ -9,7 +9,7 @@ export DEBIAN_FRONTEND=noninteractive
 rm -f /etc/apt/apt.conf.d/docker*
 apt-get -qq -y update
 apt-get -qq -y install --no-install-recommends apt-utils > /dev/null
-apt-get -qq -y install --no-install-recommends wget git > /dev/null
+apt-get -qq -y install --no-install-recommends wget curl git > /dev/null
 
 wget -c -nv --no-check-certificate -O codecov https://codecov.io/bash
 bash ./codecov -X coveragepy -X xcode -s ./target -t "${COVERAGE_TOKEN}"
